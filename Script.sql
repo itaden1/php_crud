02b490bac7801f2e518aabb42e63325fd939e7e7
Ethan Shearer <ethanshearer@Andys-MacBook-Pro.local>
2019-12-23 16:48:57 +1100

    update sql schema

--- Script.sql
+++ Script.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS speaker;
 
 CREATE TABLE event (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHARACTER(255),
+	name VARCHAR(255),
 	institution INT,
 	location_long DECIMAL(8,5),
 	location_lat DECIMAL(8,5),
@@ -16,24 +16,42 @@ CREATE TABLE event (
 
 CREATE TABLE institution (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHARACTER(255),
-	address VARCHARACTER(255)
+	name VARCHAR(255),
+	address VARCHAR(255)
 );
 
 CREATE TABLE timeslot (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	start_time TIME,
-	finish_time TIME
+	finish_time TIME,
+	timeslot_activity_id INT,
+	FOREIGN KEY(timeslot_activity)
+		REFERENCES timeslot_activity(timeslot_activity_id)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE speaker (
+CREATE TABLE timeslot_activity (
+	activity_id INT,
+	timeslot_id INT	
+);
+
+CREATE TABLE film (
+ 	title VARCHAR(255),
+ 	release_date DATE,
+ 	synopsis TEXT,
+ 	image VARCHAR(255),
+ );
+
+
+
+CREATE TABLE person (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHARACTER(255),
-	last_name VARCHARACTER(255),
-	title VARCHARACTER(255),
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	title VARCHAR(255),
 	biography TEXT,
 	institution INT,
-	photo VARCHARACTER(255),
+	photo VARCHAR(255),
 	FOREIGN KEY(institution)
 		REFERENCES institution(id)
 		ON DELETE SET NULL
